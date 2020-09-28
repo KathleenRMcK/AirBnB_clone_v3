fe197e3280d28f7af910975af70d70bf2cd55415
matxa <42968545+matxa@users.noreply.github.com>
2020-09-27 22:54:41 -0400

    add and remove amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,37 @@
-console.log($('input').select().data.id)
+const $ = window.$;
+
+/* Run script only after page is done loading */
+$(document).ready(() => {
+  /* get all input tags */
+  const checkboxes = $('input[type=checkbox]');
+  /* amenities h4 */
+  const preferences = $('.preferences');
+  /* list to hold the amenity id(s) */
+  const amenityIds = [];
+  /* list to hold the aminities to be added to the h4 .preferences */
+  const addToH4 = [];
+
+  /* detect change on checkbox */
+  $(checkboxes).change(() => {
+    /* iterate through the list of inputs */
+    $(checkboxes).each((index) => {
+      /* add to AmenityIds list and addToH4 list && remove from AmenityIds list and remove from addToH4 list */
+      if ($(checkboxes[index]).prop('checked') && amenityIds.indexOf($(checkboxes[index]).attr('data-id')) === -1 && addToH4.indexOf($(checkboxes[index]).attr('data-name')) === -1) {
+        amenityIds.push($(checkboxes[index]).attr('data-id'));
+        addToH4.push($(checkboxes[index]).attr('data-name'));
+      } else if ($(checkboxes[index]).prop('checked') === false && amenityIds.indexOf($(checkboxes[index]).attr('data-id')) > -1 && addToH4.indexOf($(checkboxes[index]).attr('data-name')) > -1) {
+        amenityIds.splice(index, 1);
+        addToH4.splice(index, 1);
+      }
+    });
+
+    /* concatinate all preferences in addToH4 as string */
+    let str = '';
+    $(addToH4).each((i) => {
+      str = str + ' ' + addToH4[i] + ',';
+    });
+
+    /* change the amenity h4 text */
+    preferences.text(str);
+  });
+});
